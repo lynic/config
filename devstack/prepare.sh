5a917ea6b4fa1044d2b994dc5068f892d6314a3e
elynn <elynn@localhost.localdomain>
2015-03-05 21:37:01 +0800

    Update prepare.sh

--- devstack/prepare.sh
+++ devstack/prepare.sh
@@ -6,6 +6,8 @@ sudo systemctl start mariadb.service
 sudo systemctl start ipsec.service
 sudo systemctl start httpd.service
 sudo losetup -f /opt/stack/data/stack-volumes-lvmdriver-1-backing-file
+sudo losetup -f /opt/stack/data/stack-volumes-default
+sudo losetup -f /opt/stack/data/stack-volumes-default-backing-file
 sudo ifconfig br-ex 172.24.4.1/24 up
 sudo ifconfig br-phy up
 sudo ifconfig br-int up
@@ -16,4 +18,4 @@ sudo iptables -I FORWARD -s 172.24.4.0/24 -j ACCEPT
 sudo iptables -D FORWARD -d 172.24.4.0/24 -j ACCEPT
 sudo iptables -I FORWARD -d 172.24.4.0/24 -j ACCEPT
 sudo ip route add 10.0.0.0/24 via 172.24.4.2 dev br-ex
-/home/elynn/source/devstack/rejoin-stack.sh
+/home/elynn/devstack/rejoin-stack.sh
