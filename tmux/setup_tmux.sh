e2e4918ca543701cf8f17be46329491938b21c5e
elynn <elynn@localhost.localdomain>
2015-05-10 18:01:03 +0800

    Update

---
+++ tmux/setup_tmux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo yum install -y tmux tmux-powerline
+cp /home/elynn/source/config/tmux/tmux.conf ~/.tmux.conf
+cp /home/elynn/source/config/tmux/tmux.conf.local ~/.tmux.conf.local
