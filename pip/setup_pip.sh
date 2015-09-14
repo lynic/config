08dd030e2cc79bc7fd20aeec34b3f80f57a5f7ef
elynn <elynn@elynn-78.cn.ibm.com>
2015-09-14 12:01:45 +0800

    update setup_pip.sh

--- pip/setup_pip.sh
+++ pip/setup_pip.sh
@@ -5,3 +5,8 @@ if [[ ! -d ~/.pip ]]; then
   mkdir -p ~/.pip/cache
 fi
 cp pip.conf ~/.pip/
+
+if sudo bash -c '[[ ! -d /root/.pip ]]'; then
+  sudo mkdir -p /root/.pip/cache
+fi
+sudo cp pip.conf /root/.pip/
