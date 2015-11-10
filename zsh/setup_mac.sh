5f6b3757073eae527fb6c2c9911e307ea4a750bc
EthanLynn <EthanLynn@EthanLynns-MacBook-Pro.local>
2015-11-11 01:02:32 +0800

    Updaate

--- zsh/setup_mac.sh
+++ zsh/setup_mac.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 
+# Install HomeBrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh tmux brew-cask
+brew install zsh tmux brew-cask vagrant git
+brew install virtualbox virtualbox-extension-pack
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
