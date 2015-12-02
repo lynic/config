88dad31d90bf690d6dd55acf21e3c4157af13f5f
elynn <elynn@localhost.localdomain>
2015-12-02 19:42:14 +0800

    Update

--- tmux/setup_tmux.sh
+++ tmux/setup_tmux.sh
@@ -6,3 +6,12 @@ cp /home/elynn/source/config/tmux/tmux.conf ~/.tmux.conf
 cp /home/elynn/source/config/tmux/tmux.conf.local ~/.tmux.conf.local
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+cat >> ~/.zshrc <<END
+
+    alias tmux='tmux -2'
+    alias ta='tmux attach -t'
+    alias tn='tmux new -s'
+    alias tl='tmux ls'
+    alias tk='tmux kill-session -t'
+END
