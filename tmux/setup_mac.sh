8526f9f6e32e50500d160bacf1158958f4a3b5cf
EthanLynn <EthanLynn@EthanLynns-MacBook-Pro.local>
2015-12-04 01:11:25 +0800

    Update

--- tmux/setup_mac.sh
+++ tmux/setup_mac.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+set -x
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install HomeBrew
+if [[ -z `command -v brew` ]];then
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install zsh tmux brew-cask vagrant git
 brew install virtualbox virtualbox-extension-pack
+fi 
 
 # Install oh-my-zsh
+if [[ ! -f ~/.zshrc ]];then
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+fi
 
 # Install vim
+if [[ ! -d ~/.vim_runtime ]];then
 git clone git://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_basic_vimrc.sh
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+cp $DIR/../vim/vimrc ~/.vim_runtime/my_configs.vim
+fi
