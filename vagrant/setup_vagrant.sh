3de77fd87dcdc7901e16948db0758f029413ce00
elynn <elynn@localhost.localdomain>
2016-01-02 09:02:59 +0800

    Update

--- vagrant/setup_vagrant.sh
+++ vagrant/setup_vagrant.sh
@@ -4,6 +4,8 @@ set -x
 sudo yum install -y vagrant vagrant-libvirt libvirt
 sudo curl -o /etc/yum.repos.d/virtualbox.repo http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
 sudo yum install -y VirtualBox-5.0
+sudo yum install -y kernel-devel-`uname -r`
+sudo KERN_DIR=/usr/src/kernels/`uname -r`/ /sbin/rcvboxdrv setup
 
 if [[ -n `command -v zsh` && -z `grep 'alias v=' ~/.zshrc` ]];then
 cat >> ~/.zshrc <<END
