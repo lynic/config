b351b93bd08186a187de78675949d015c0040b54
EthanLynn <EthanLynn@EthanLynns-MacBook-Pro.local>
2016-01-24 01:00:06 +0800

    update

--- scripts/setup_mac.sh
+++ scripts/setup_mac.sh
@@ -2,19 +2,35 @@
 set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+function brew_install {
+    for software in "$@"; do
+        if [[ -z `brew list |grep $software` ]]; then
+            brew install $software
+        fi
+    done
+}
+
+function brew_cask_install {
+    for software in "$@"; do
+        if [[ -z `brew cask list |grep $software` ]]; then
+            brew cask install $software
+        fi
+    done
+}
+
 # Install HomeBrew
 if [[ -z `command -v brew` ]];then
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-brew install zsh tmux
-brew install git git-review
+brew_install tmux
+brew_install git git-review
 git config --global core.editor vim
-brew install docker docker-machine docker-compose
+brew_install docker docker-machine docker-compose
 
 brew tap caskroom/cask
-brew cask install iterm2
-brew cask install vagrant
-brew cask install virtualbox virtualbox-extension-pack
+brew_cask_install iterm2
+brew_cask_install vagrant
+brew_cask_install virtualbox virtualbox-extension-pack
 
 cp $DIR/../tmux/tmux.conf ~/.tmux.conf
 cp $DIR/../tmux/tmux.conf.local.mac ~/.tmux.conf.local
@@ -22,22 +38,22 @@ cp $DIR/../tmux/tmux.conf.local.mac ~/.tmux.conf.local
 # Install oh-my-zsh
 if [[ ! -f ~/.zshrc ]];then
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
-# echo 'eval "$(rbenv init -)"' >> ~/.zshrc
-# echo 'export HOMEBREW_GITHUB_API_TOKEN=71135fbace8ee405108617aea062824b80461def' >> ~/.zshrc
+fi
+cp -f ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 echo "export VAGRANT_DEFAULT_PROVIDER='virtualbox'" >> ~/.zshrc
 cat >> ~/.zshrc <<END
 function endm {
 docker-machine start \$1
 eval \$(docker-machine env \$1)
 }
-alias lsdm='docker-machine ls'
+alias dm='docker-machine'
+alias vup='vagrant up'
+alias vls='vagrant global-status'
 END
-fi
 
 # Install vim
 if [[ ! -d ~/.vim_runtime ]];then
-git clone git://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+    git clone git://github.com/amix/vimrc.git ~/.vim_runtime
+    sh ~/.vim_runtime/install_awesome_vimrc.sh
 fi
 cp $DIR/../vim/vimrc ~/.vim_runtime/my_configs.vim
