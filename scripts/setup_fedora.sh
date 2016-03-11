8ea257c9dd9699372da2bbe6f184863be7916829
elynn <elynn@localhost.localdomain>
2016-03-11 08:26:32 +0800

    Add rpmfusion repo

--- scripts/setup_fedora.sh
+++ scripts/setup_fedora.sh
@@ -6,6 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo setenforce 0
 sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 
+# enable RPMFusion
+sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 $DIR/../zsh/setup_zsh.sh
 $DIR/../tmux/setup_tmux.sh
 $DIR/../vim/setup_vim.sh
