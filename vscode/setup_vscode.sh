4962f9978b73cd0cdd63e1aa00e887dce8e7f438
elynn <elynn@localhost.localdomain>
2016-03-30 17:56:23 +0800

    Update vscode script

--- vscode/setup_vscode.sh
+++ vscode/setup_vscode.sh
@@ -4,8 +4,11 @@ DESKTOP_DIR=$HOME/.local/share/applications/
 CONFIG_DIR=$HOME/.config/Code/User/
 
 # Download package
+curl -L https://go.microsoft.com/fwlink/?LinkID=620884 -o /tmp/vscode.zip
+unzip /tmp/vscode.zip -d /tmp
 
 # Unpack to ~/Documents/
+mv /tmp/VSCode-linux-x64 $HOME/Documents/vscode
 
 if [[ ! -d $DESKTOP_DIR ]];then
     mkdir -p $DESKTOP_DIR
