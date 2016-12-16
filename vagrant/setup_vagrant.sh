f722ba63fcd6a9a98a19a81e0b650bf820940ff1
Ethan Lynn <xuanlangjian@gmail.com>
2016-12-16 17:30:23 +0800

    Install libvirt plugin with vagrant

--- vagrant/setup_vagrant.sh
+++ vagrant/setup_vagrant.sh
@@ -2,23 +2,21 @@
 set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VAGRANT_VERSION="1.9.1"
-VB_VERSION=5.1
+VB_VERSION="5.1"
 
-# sudo curl -o /etc/yum.repos.d/virtualbox.repo http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo
+# Install VirtualBox
 cd /etc/yum.repos.d/
 sudo curl -L http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -O
 sudo yum install -y kernel-devel-`uname -r`
 sudo yum install -y VirtualBox-${VB_VERSION}
-#sudo KERN_DIR=/usr/src/kernels/`uname -r`/ /sbin/rcvboxdrv setup
-# /sbin/vboxconfig
 sudo usermod -aG vboxusers $(whoami)
 
+# Install vagrant
 curl -L https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.rpm -o /tmp/vagrant.rpm
 cd /tmp/
 sudo yum install ./vagrant.rpm
 
-sudo yum install -y libvirt
-
-# sudo yum install -y kernel-devel-`uname -r`
-# sudo yum install -y VirtualBox-${VB_VERSION}
-#sudo KERN_DIR=/usr/src/kernels/`uname -r`/ /sbin/rcvboxdrv setup
+# Install libvirt
+sudo yum install -y qemu libvirt libvirt-devel ruby-devel gcc qemu-kvm
+sudo usermod -aG libvirt $(whoami)
+vagrant plugin install vagrant-libvirt
