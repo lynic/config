bb04ea5310e326b206612cd0fc6c6f55e60918cc
Ethan Lynn <xuanlangjian@gmail.com>
2016-12-20 07:00:32 +0800

    Update

--- vscode/setup_vscode.sh
+++ vscode/setup_vscode.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DESKTOP_DIR=$HOME/.local/share/applications/
-CONFIG_DIR=$HOME/.config/Code/User/
+DESKTOP_DIR=$HOME/.local/share/applications
+CONFIG_DIR=$HOME/.config/Code/User
 
 # Download package
 curl -L https://go.microsoft.com/fwlink/?LinkID=760867 -o /tmp/vscode.rpm
 cd /tmp/
 sudo yum install ./vscode.rpm -y
-ln `pwd`/settings.json $CONFIG_DIR/settings.json
+ln $DIR/settings.json $CONFIG_DIR/settings.json
 
