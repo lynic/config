77976a3dfdaefcb8e92b53cbe2549ecf4d7c0cfa
Ethan Lynn <elynn@Ethans-MBP.lan>
2017-01-05 22:58:08 +0800

    Add tmux for mac

---
+++ tmux/setup_mac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+brew install python tmux
+pip install --user powerline-status
+
+echo 'export LC_ALL=en_US.UTF-8' >> ~/.zshrc
+echo 'export LANG=en_US.UTF-8' >> ~/.zshrc
+
+POWERLINE_CONF=$(find $HOME -name powerline.conf |head -n 1)
+POWERLINE_CMD=$(find $HOME -name powerline-config | head -n 1)
+
+echo "export POWERLINE_CONFIG_COMMAND=$POWERLINE_CMD" >> ~/.zshrc
+
+cp ./tmux.conf ~/.tmux.conf
+cp ./tmux.conf.local.mac ~/.tmux.conf.local
+echo "source $POWERLINE_CONF" >> ~/.tmux.conf.local
+
+git clone git clone https://github.com/powerline/fonts /tmp/fonts
+cd /tmp/fonts
+./install.sh
--- tmux/setup_tmux.sh
+++ tmux/setup_tmux.sh
@@ -22,16 +22,3 @@ fi
 cp $DIR/tmux.conf ~/.tmux.conf
 cp $DIR/tmux.conf.local ~/.tmux.conf.local
 
-if [[ -z "`grep 'tmux begin' ~/.zshrc`" ]];then 
-cat >> ~/.zshrc <<END
-
-### tmux begin ###
-alias tmux='tmux -2'
-alias ta='tmux attach -t'
-alias tn='tmux new -s'
-alias tl='tmux ls'
-alias tk='tmux kill-session -t'
-### tmux end ###
-END
-fi
-
