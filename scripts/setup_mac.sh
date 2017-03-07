9ee35bb86c1556cf9f97cd46d14dcea5e652ab9f
Ethan Lynn Lin <ethanlynnl@ethanlynnl-m01.vmware.com>
2017-03-08 00:32:24 +0800

    update

--- scripts/setup_mac.sh
+++ scripts/setup_mac.sh
@@ -20,41 +20,47 @@ function brew_cask_install {
 
 # Install HomeBrew
 if [[ -z `command -v brew` ]];then
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+# Install needed tools
 brew_install tmux
+brew_install python
 brew_install git git-review
 git config --global core.editor vim
-brew_install docker docker-machine docker-compose
 
-brew tap caskroom/cask
 brew_cask_install iterm2
 brew_cask_install vagrant
 brew_cask_install virtualbox virtualbox-extension-pack
-
-pip install --user powerline-status
-POWERLINE_CONF=$(find $HOME -name powerline.conf |head -n 1)
-POWERLINE_CMD=$(find $HOME -name powerline-config | head -n 1)
-cp $DIR/../tmux/tmux.conf ~/.tmux.conf
-cp $DIR/../tmux/tmux.conf.local.mac ~/.tmux.conf.local
-sed -i "s/POWERLINE_CONF/$POWERLINE_CONF" ~/.tmux.conf.local
-sed -i "s/POWERLINE_CMD/$POWERLINE_CMD" ~/.tmux.conf.local
 
 # Install oh-my-zsh
 if [[ ! -f ~/.zshrc ]];then
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 cp -f ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-echo "export VAGRANT_DEFAULT_PROVIDER='virtualbox'" >> ~/.zshrc
-cat >> ~/.zshrc <<END
-function endm {
-docker-machine start \$1
-eval \$(docker-machine env \$1)
-}
-alias dm='docker-machine'
-alias vup='vagrant up'
-alias vls='vagrant global-status'
-END
+if [[ -z `grep VAGRANT_DEFAULT_PROVIDER ~/.zshrc` ]];then
+    echo "export VAGRANT_DEFAULT_PROVIDER='virtualbox'" >> ~/.zshrc
+fi
+
+
+# Install powerline
+pip install --user powerline-status
+# POWERLINE_CONF=$(find $HOME -name powerline.conf |head -n 1)
+# POWERLINE_CMD=$(find $HOME -name powerline-config | head -n 1)
+cp $DIR/../tmux/tmux.conf ~/.tmux.conf
+cp $DIR/../tmux/tmux.conf.local.mac ~/.tmux.conf.local
+if [[ -z `grep POWERLINE_CONFIG_COMMAND ~/.zshrc` ]]; then
+  echo 'export POWERLINE_CONFIG_COMMAND=/Users/$USER/Library/Python/2.7/bin/powerline-config' >> ~/.zshrc
+fi
+#sed -i "s/POWERLINE_CONF/$POWERLINE_CONF" ~/.tmux.conf.local
+#sed -i "s/POWERLINE_CMD/$POWERLINE_CMD" ~/.tmux.conf.local
+# Install fonts
+if [[ ! -d ~/Documents/fonts ]];then
+    cd ~/Documents/
+    git clone https://github.com/powerline/fonts
+    cd fonts
+    ./install.sh
+fi
 
 # Install vim
 if [[ ! -d ~/.vim_runtime ]];then
